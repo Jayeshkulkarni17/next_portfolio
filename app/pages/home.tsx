51c2e953508ba6be4235cccbbb4a590f16fe3b20
jayeshkulkarni17 <jaykulkarni17ab@gmail.com>
2024-10-20 15:12:46 +0530

    setup and background

---
+++ app/pages/home.tsx
@@ -0,0 +1,14 @@
+import { BackgroundBeamsWithCollision } from "../components/ui/background";
+import { Menu } from "../components/ui/navbar-menu";
+
+export default function Home() {
+  function setActiveItem(item: string | null): void {
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <>
+      <BackgroundBeamsWithCollision children={undefined} />
+    </>
+  );
+}
